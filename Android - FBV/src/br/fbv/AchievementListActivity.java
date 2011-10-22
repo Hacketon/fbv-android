9e653a381047193771be894f08bc3b1dae133c47
fehaa2000 <fehaa2000@gmail.com>
2011-10-22 23:40:02 +0000

    Activities adicionadas.

---
+++ Android - FBV/src/br/fbv/AchievementListActivity.java
@@ -0,0 +1,8 @@
+package br.fbv;
+
+import android.app.Activity;
+
+public class AchievementListActivity extends Activity 
+{
+
+}
--- Android - FBV/src/br/fbv/MainActivity.java
+++ Android - FBV/src/br/fbv/MainActivity.java
@@ -3,10 +3,12 @@ package br.fbv;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
