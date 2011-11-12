136798b73776d2709a8001d806d884af532aecd4
fehaa2000 <fehaa2000@gmail.com>
2011-11-12 12:58:06 +0000

    Novos Layouts adicionados.

--- Android - FBV/src/br/fbv/MainActivity.java
+++ Android - FBV/src/br/fbv/MainActivity.java
@@ -4,29 +4,13 @@ import br.fbv.account.NewAccountActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class MainActivity extends Activity implements OnClickListener
+public class MainActivity extends Activity 
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) 
-    {
+	Intent i = new Intent( this, NewAccountActivity.class);
+
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.main);
-        
-        final Button btnCadUser = (Button) findViewById(R.id.btnCadUser);
-        btnCadUser.setOnClickListener(this);
-        
-    } // end method onCreate
-
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		startActivity(new Intent(this, NewAccountActivity.class));
-		
-	} // end method onClick
-        
-} // end class MainActivity
+        startActivity(i);
+	}
+}
--- Android - FBV/src/br/fbv/notes/NoteListActivity.java
+++ Android - FBV/src/br/fbv/notes/NoteListActivity.java
@@ -1,8 +1,22 @@
 package br.fbv.notes;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class NoteListActivity extends Activity
 {
-
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.notes);
+/**
+        Button next = (Button) findViewById(R.id.botao02);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+        */
+    }
 }
