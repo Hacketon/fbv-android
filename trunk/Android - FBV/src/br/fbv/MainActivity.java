a2400dc9d2c38e89ea89732332667f5d12e297e7
fehaa2000@gmail.com <fehaa2000@gmail.com@fec3a432-b8bb-4522-8a44-a6a7d434937b>
2011-11-12 19:35:47 +0000

    Editando MainActivity.

--- trunk/Android - FBV/src/br/fbv/MainActivity.java
+++ trunk/Android - FBV/src/br/fbv/MainActivity.java
@@ -1,16 +1,27 @@
 package br.fbv;
 
-import br.fbv.account.NewAccountActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import br.fbv.account.NewAccountActivity;
 
 public class MainActivity extends Activity 
 {
-	Intent i = new Intent( this, NewAccountActivity.class);
-
-	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        startActivity(i);
+	Intent it = new Intent( this, NewAccountActivity.class);
+	
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		Button btnLogin = (Button) findViewById( R.id.botaoWelcome);
+		
+		btnLogin.setOnClickListener( new View.OnClickListener() {
+			public void onClick(View v) {
+				startActivity(it);
+			}
+		});
 	}
 }
